66c655126bafb3d3febc4dede9e33ed62211517c
Anthony Leatherwood <adleatherwood@gmail.com>
2018-07-12 18:44:17 +0000

    refactor(library): Moved to .net standard

---
+++ src/DrivenDb.MySql.Tests/README.md
@@ -0,0 +1,4 @@
+﻿# TEST SETUP
+
+## Docker command
+ docker run --rm --hostname mysql --name mysql -p3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:8.0.11
---
+++ src/DrivenDb.Tests/Assert.Ex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DrivenDb.Tests
+{
+    internal static class AssertX
+    {
+        public static void Throws<T>(Action a)
+            where T : Exception
+        {
+            try
+            {
+                a();
+            }
+            catch(Exception e)
+            {
+                if (e.GetType() != typeof(T))
+                    Assert.Fail();
+            }
+        }
+    }
+}
